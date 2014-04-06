e877da21d26aca6c5ac733bf332bd5f6b763214a
b87775 <jwoods1@fullsail.edu>
2014-04-05 22:22:58 -0600

    added Expression Worksheet

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,42 @@
+/*
+  Woods Jason
+  April 5 2014
+  Expressions Worksheet
+*/
+/* Examples
+//Calculate the area of a rectangle given the width and height of the rectangle.
+// rectangle value
+var recWidth = 15;
+var recHeight = 20;
+
+// Calculate Area of a rectangle 
+var recArea = recWidth * recHeight;
+
+//logging the result to console
+console.log('The Area of the Rectangle is: ' + recArea);
+
+// Calculate the circumference of the circle
+//constent 
+var Pi = 3.14159265;
+// circle values
+var radius = 10;
+// equation for circumference
+var circum = 2*Pi*radius;
+
+console.log('The circumference of the circle is: '+circum);
+*/
+
+//Dog Years
+//constent for dog years
+var dogYears = 7;
+//how many years old the dog is in human years.
+var humanYears = 3;
+//human to dog year equation
+var sparkyAge = dogYears * humanYears;
+//log to the console. 
+console.log('Sparky is ' + humanYears + ' years old which is '+ sparkyAge + ' in dog years.');
+
+
+
+
+
