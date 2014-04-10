a87fd1ee602de9d583d01c8502baee8f27090d75
b87775 <jwoods1@fullsail.edu>
2014-04-09 22:35:24 -0600

    Added alert with array length to wacky expressions

--- Week2/Expressions_Wacky/js/script.js
+++ Week2/Expressions_Wacky/js/script.js
@@ -1,9 +1,19 @@
 /*
  * Woods Jason
+ * April 9 2014
  * WPF 201404 01
  * Expressions Wacky
  */
 // JavaScript
-alert('this is in the js file');
+alert('*****ThIs Is ThE WaCkY! Js FiLe *******');
 //this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+document.querySelector("body").style.backgroundColor= "#3D8077";
+
+/*
+* How many things are in an array.
+*/
+var wackyPromt = prompt('Add A wacky String!:D');
+var Array = ['OnE WaCkY ThIng!!:>)','TwO WaXy Things!:D',wackyPromt];
+//Tells the length of the array.
+alert('The Array has '+Array.length+' Objects in it!:D');
+console.log('The Array has '+Array.length+' Objects in it!:D');
