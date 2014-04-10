7749c982d96d3ca7f874af3650ba1ac83607c822
b87775 <jwoods1@fullsail.edu>
2014-04-09 22:45:15 -0600

    added array of number and added them together

--- Week2/Expressions_Wacky/js/script.js
+++ Week2/Expressions_Wacky/js/script.js
@@ -21,3 +21,13 @@ console.log('The Array has '+Array.length+' Objects in it!:D');
 //list array object wacky!
 alert('Objects are wacky! Here is the list of array objects: '+Array[0]+', '+Array[1]+', '+Array[2]+'.');
 console.log('Objects are wacky! Here is the list of array objects: '+Array[0]+', '+Array[1]+', '+Array[2]+'.');
+
+//Add Some things together!
+//have user enter a number
+var enteredNum = Number(prompt('Enter a number Any Number!'));
+// add number to array
+var numberS = [100, 300, 600, enteredNum];
+// add the numbers.
+var addNums = numberS[1] + numberS[0] + numberS[2] + numberS[3];
+console.log('The numbers added togehter are:'+addNums);
+alert('The numbers added togehter are:'+addNums);
