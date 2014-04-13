fe065b15b811be1288382e3f477bf4acea092604
b87775 <jwoods1@fullsail.edu>
2014-04-13 09:48:06 -0600

    added title to Conditionals Worksheet JS file.

--- week 3/Conditionals_worksheet/js/script.js
+++ week 3/Conditionals_worksheet/js/script.js
@@ -1,4 +1,9 @@
-// JavaScript
+/*
+Jason Woods
+2014 04 13
+WPF 201404 S01
+Conditionals Worksheet
+*/// JavaScript
 alert('this is in the js file');
 //this changes the css of your body tag to be green
 document.querySelector("body").style.backgroundColor= "#00FF00";
