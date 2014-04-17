17a16f6ae91cb438972557d8b2ec5896ba5a29c5
b87775 <jwoods1@fullsail.edu>
2014-04-16 22:22:22 -0600

    added troubleshooting to industry conditionals

--- week 3/Conditionals_Industry/js/script.js
+++ week 3/Conditionals_Industry/js/script.js
@@ -12,3 +12,15 @@
  A logical operator
  At least one relational operator other than ==
  */
+
+//Electrical wire troubleshooting.
+var troubleshoot = prompt("Do you need to troubleshoot a wire? yes or no");
+if(troubleshoot == 'yes'){
+    //Get the ohms of a wire.
+    var ohms = Number(prompt("What is the current omhs of wire 10?"));
+// if ohms is blank
+    if(ohms == ''){
+        var oops = Number(prompt("You must enter a reading of omhs"));
+        ohms = oops;
+    };
+}
