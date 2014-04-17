3c4b1e23dd2a66d760a53fcdb3485710e04b909d
b87775 <jwoods1@fullsail.edu>
2014-04-16 22:10:38 -0600

    added if else if stament to wacky conditionals

--- week 3/Conditionals_Wacky/js/script.js
+++ week 3/Conditionals_Wacky/js/script.js
@@ -16,15 +16,31 @@
 var dogName = prompt("What is your dogs name?");
 //if statement to see if the prompt is blank
 if(dogName == ''){
-    Alert('You must enter a dogs name!');
+    alert('You must enter a dogs name!');
     dogName = prompt("What is your dogs name?");
 };
 //get current time
 var curTime = Number(prompt("What is the current time?"));
 //if statemtent to see if prompt is blank
 if(curTime == ''){
-    Alert('you must enter a Time');
+    alert('you must enter a Time');
     curTime = Number(prompt("What is the current time?"));
 }
 //Get the time interval for potty training
 var pottyTime = Number(prompt("How often do you want to take out "+dogName+'?'));
+//if blank statement
+if(pottyTime == ''){
+    alert("You didn't put a time that you want to take out "+dogName+", This is required");
+    pottyTime = Number(prompt("How often do you want to take out "+dogName+'?'));
+}
+
+// if potty time is greater then 20 min give time tell next potty.
+if(pottyTime >= 20){
+    //add times to get new time for potty.
+    var timeToPotty = curTime + pottyTime;
+    //log results
+    console.log(dogName+" needs to go potty at "+timeToPotty+" hundred hours!");
+    alert(dogName+" needs to go potty at "+timeToPotty+" hundred hours!");
+}else if(pottyTime <= 19){
+    console.log(pottyTime+' is not enough time for your dog to even want to go potty. Relax and give '+dogName+' at least 20min!');
+};
