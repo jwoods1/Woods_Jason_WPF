cf8aebd0a1f0aab803fb2fb85c7502573081d840
b87775 <jwoods1@fullsail.edu>
2014-04-16 22:26:55 -0600

    fixed if troubleshoting is blank on conditional industry

--- week 3/Conditionals_Industry/js/script.js
+++ week 3/Conditionals_Industry/js/script.js
@@ -15,12 +15,19 @@
 
 //Electrical wire troubleshooting.
 var troubleshoot = prompt("Do you need to troubleshoot a wire? yes or no");
+if(troubleshoot == ''){
+  var oops = prompt("You have to answer yes or no if you need to troubleshoot");
+  troubleshoot = oops;
+};
 if(troubleshoot == 'yes'){
     //Get the ohms of a wire.
     var ohms = Number(prompt("What is the current omhs of wire 10?"));
 // if ohms is blank
     if(ohms == ''){
-        var oops = Number(prompt("You must enter a reading of omhs"));
+         oops = Number(prompt("You must enter a reading of omhs"));
         ohms = oops;
     };
+}else{
+    alert("Well I'm glade everything works!");
+    console.log("Well I'm glade everything works!");
 }
