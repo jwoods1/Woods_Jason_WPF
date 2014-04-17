58bd453915d6a437a6a600ac4d46fc685ee252c2
b87775 <jwoods1@fullsail.edu>
2014-04-16 21:52:12 -0600

    added dogName var with if statement to wacky conditionals

--- week 3/Conditionals_Wacky/js/script.js
+++ week 3/Conditionals_Wacky/js/script.js
@@ -11,3 +11,13 @@
  A logical operator
  At least one relational operator other than ==
     */
+
+// Dog the dog have to go potty?
+var dogName = prompt("What is your dogs name?");
+//if statement to see if the prompt is blank
+if(dogName == ''){
+    Alert('You must enter a dogs name!');
+    dogName = prompt("What is your dogs name?");
+};
+
+
