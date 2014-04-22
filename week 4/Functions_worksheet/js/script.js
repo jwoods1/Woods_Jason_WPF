35ca88ce021421f8f87671e0b706adc3bb699011
b87775 <jwoods1@fullsail.edu>
2014-04-21 21:47:49 -0600

    added problem #2

--- week 4/Functions_worksheet/js/script.js
+++ week 4/Functions_worksheet/js/script.js
@@ -33,3 +33,18 @@ var radius = Number(prompt("What is the radius?"));
 // Put the function inside of a var to hold the value.
 var cir = circumference(radius);
 console.log("The circumference of the circle is "+cir);
+
+
+/* Problem # 2 Stung!
+
+ It takes 8.666666667 bee stings per pound to kill an animal. Calculate how many bee stings are needed to kill an animal in a function
+ Given:
+ Victim’s weight (in pounds)
+ Parameter(s) for function:
+ Victim’s weight (in pounds)
+ Return:
+ Number of Bee stings
+ Result to print to the console:
+ “It takes X bee stings to kill this animal.*/
+
+// c
