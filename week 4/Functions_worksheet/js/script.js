958917fbc3cbc98b2d0f69f6c296a14ed456e798
b87775 <jwoods1@fullsail.edu>
2014-04-21 21:33:59 -0600

    added title and example to week 4 Functions worksheet .js

--- week 4/Functions_worksheet/js/script.js
+++ week 4/Functions_worksheet/js/script.js
@@ -1,4 +1,24 @@
-// JavaScript
-alert('this is in the js file');
-//this changes the css of your body tag to be green
-document.querySelector("body").style.backgroundColor= "#00FF00";
+/*
+    Woods, Jason
+    Week 4 Functional Worksheet
+    21 April 2014
+    WPF 0414 01
+*/
+
+/*    Example:
+
+ Problem: Calculate the area of a rectangle given the width and height of the rectangle.
+ //Calculate the Area of a Rectangle
+ var width = 5;
+ var height = 6;
+ var area = calculateArea(width, height);
+ console.log(“The Area of the Rectangle is “ + area);
+ private function calculateArea(w, h ):Number
+ {
+ return w *h;
+ }
+ */
+
+// Problem #1   Circumference
+
+// Create function with parameters for equation.
