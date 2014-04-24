84516b68d78dddb58bcdfa8b1745c0cd2d7d5ad8
b87775 <jwoods1@fullsail.edu>
2014-04-23 20:28:48 -0600

    Added Main folders for week 4 main assignment.

---
+++ week 4/Functions_Industry/js/script.js
@@ -0,0 +1,19 @@
+/*
+ Jason Woods
+ 20140422
+ WPF 201404 S01
+ Functions Industry
+ */
+
+
+/*
+ Each of the following requirements must exist in at least one of the 3 subprojects:
+ A ternary
+ An else if
+ An anonymous function
+ A normal "named" function
+ An expression with two arithmetic operators
+ A function with three parameters.
+ At least one logical operator
+
+ */
