0a1637af6fdf6c7c2d0ed72d31aba6b886c505ec
b87775 <jwoods1@fullsail.edu>
2014-04-23 22:09:41 -0600

    functional tested with personal functions.

--- week 4/Functions_Personal/js/script.js
+++ week 4/Functions_Personal/js/script.js
@@ -11,28 +11,34 @@
  A ternary
  An else if
  An anonymous function
- A normal "named" function
- An expression with two arithmetic operators
- A function with three parameters.
- At least one logical operator
+1 A normal "named" function
+1 An expression with two arithmetic operators
+1 A function with three parameters.
+1 At least one logical operator
 
  */
-//Function to figure out how much time i have for homework a week.
+//normal named function
+//Function to figure out how much time i have for homework a week.  function with three parameters.
 function homeWorkTime(time,assignments,work){
+    // expression with 2 operators
     // take the time in a day that you don't sleep and subtract work to get time left.
     var timeLeft = time - work;
+    //see how much time is needed.
+    var timeNeeded = assignments * 1.5;
     // see if it is enough time.
-    if(timeLeft >= 1 ){
-        //see how much time is needed.
-        var timeNeeded = assignments * 1.5;
+    if(timeLeft > 1 && timeNeeded <=timeLeft){
         // see what time is left over after homework.
         var totalT = timeLeft - timeNeeded;
         var timeLeftOver = "there is "+totalT+" hrs left over after homework is complete";
         return timeLeftOver;
     }else{
         var notEnoughTime = "There is not enought time it will require "+timeLeft+" more hours in the day.";
-        return timeLeft;
+        return notEnoughTime;
     }
 }
-var homework = homeWorkTime(18,3,10);
-console.log(homework);
+
+var homework = prompt("How much home work do you have?");
+var timeInDay = prompt("How much time do you have in the day that you don't sleep?");
+var workDay = prompt("How long is your work day?");
+
+console.log(homeWorkTime(timeInDay,homework,workDay));
